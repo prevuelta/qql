2fce2688f84c2b4e954e39c4dc342433ae1c66c1
Pablo Revuelta <hello@pablorevuelta.com>
2024-12-21 07:32:15 +1300

    wip-1734719535

--- src/app.ts
+++ src/app.ts
@@ -5,23 +5,37 @@ const grammarPath = path.join(import.meta.dirname, "./gglGrammar.ohm");
 
 const contents = fs.readFileSync(grammarPath, "utf8");
 
-const gglGrammar = ohm.grammar(`
+const gglGrammar = ohm.grammar(
+  String.raw`
  Ggl <: IndentationSensitive {
-   Command = CommandIdentifier ":" CommandArguments
-   CommandIdentifier = "d" | ""
-   CommandArguments = CommandArg*
-   CommandArg 
+   Program = CommandAndArgs+
+   CommandAndArgs = Indent Command ":" CommandArguments eol?
+   Command = "d" --documentCommand
+   		| "sg" --squareGridCommand
+      | "p" --pathCommand
+   Indent = (space | newline)*
+   CommandArguments = Arg*
+   Arg = singleArg | doubleArg
+   singleArg = ("x" | "y" | "u" | "a" | digit)+ ","?
+   doubleArg = singleArg singleArg ","
+   eol = newline | end
+   newline = "\\r\\n" | "\\r" | "\\n"
  }
-`, { IndentationSensitive: ohm.ExperimentalIndentationSensitive});
+`,
+  { IndentationSensitive: ohm.ExperimentalIndentationSensitive },
+);
 
 const userInput = `
-d:a a 0
+d:a a 10
+ sg:2 4 10 4
+ p:1u 0, 0 1u
+ p:0 0,1uxy
 `;
 
 const m = gglGrammar.match(userInput);
 
 if (m.succeeded()) {
-  console.log("Greetings, human.");
+  console.log("Success!");
 } else {
-  console.log("That's not a greeting!");
+  console.log("-----\nFailure", m.message);
 }
